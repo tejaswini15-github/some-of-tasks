01b12c184b6e4f96866491b382d162110a9ae14d
tejaswini15-github <pendotatejaswini2000@gmail.com>
2024-12-08 18:07:54 +0530

    some of tasks

---
+++ java script/variable day-1/index.js
@@ -0,0 +1,38 @@
+class Mobile {
+    // properties
+    camera;
+    powerButton;
+    volumeButton;
+    speaker;
+    mic;
+    logo;
+    constructor() {
+        this.camera = "200MP";
+        this.powerButton = "Metal";
+        this.volumeButton = "Plastic";
+        this.speaker = "Harmony";
+        this.mic = "Single Mic";
+        this.logo = "Apple";
+    }
+}
+var apple = new Mobile();
+var apple2 = new Mobile();
+var apple3 =new Mobile();
+console.log(apple);
+console.log(apple2);
+console.log(apple3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
